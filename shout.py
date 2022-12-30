c76e2da9c451b78bd5d8c2c4a63862185eef3ca5
raf <rraf@tuta.io>
2022-12-30 19:45:26 +0000

    update: made program to demonstrate def

---
+++ shout.py
@@ -0,0 +1,6 @@
+def shout(word):  # string here could be anything
+    print(word.upper())  # print the provided word in upercase
+
+
+word = input("enter a word: ")  # prompt the user for a word
+shout(word)  # shout the word by calling the function we defined
